bd863b4cf1c8f21c40e76ee4b4e38c404d3331e2
meshwa27 <5430meshwapatel@gmail.com>
2025-02-05 19:35:42 -0800

    update

--- 1 to n_using_forloop.cpp
+++ 1 to n_using_forloop.cpp
@@ -3,13 +3,11 @@ using namespace std;
 
 int main() {
     int n;
+
     
-    // Prompt user for input
     cout << "Enter the value of n: ";
     cin >> n;
-
-
-    // Loop to print numbers from 1 to n
+   
     for (int i = 1; i <= n; i++) {
         cout << i << endl;
     }
